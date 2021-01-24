65530d3131f72b857027031efd76ec334be081db
Danny August Ramaputra <daystram@gmail.com>
2021-01-24 15:05:34 +1000

    Updated User fields

--- src/User.ts
+++ src/User.ts
@@ -1,7 +1,8 @@
 export interface User {
-  subject: string;
+  sub: string;
   given_name: string;
   family_name: string;
   preferred_username: string;
   is_superuser: boolean;
+  created_at: number;
 }
